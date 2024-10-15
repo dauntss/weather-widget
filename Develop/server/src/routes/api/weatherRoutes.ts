6e8677ff0fd80df5eb6035b5cb2d89ddd027137a
dauntss <kat.krahulec@gmail.com>
2024-10-14 20:02:22 -0600

    Uploading the project

---
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -0,0 +1,45 @@
+import { Router, type Request, type Response } from 'express';
+const router = Router();
+import dotenv from 'dotenv';
+dotenv.config();
+import * as fs from 'fs';
+
+// TODO: POST Request with city name to retrieve weather data
+router.post('/', async (req: Request, res: Response) => {
+  console.log(req.body);
+  // TODO: GET weather data from city name
+  let weatherData = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${req.body.cityName}&limit=100&appid=${process.env.API_KEY}`);
+  let weatherDataJson = await weatherData.json();
+  let forecastData = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${req.body.cityName}&limit=100&appid=${process.env.API_KEY}`);
+  let forecastDataJson = await forecastData.json();
+  console.log(forecastDataJson);
+  console.log(weatherDataJson);
+  // TODO: save city to search history
+  let randomID = crypto.randomUUID();
+  let searchHistory = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  searchHistory.push({id: randomID, city: req.body.city});
+  fs.writeFileSync('./db/db.json', JSON.stringify(searchHistory));
+  res.json(weatherDataJson);
+});
+
+// TODO: GET search history
+router.get('/history', async (req: Request, res: Response) => {
+  let searchHistory = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  res.json(searchHistory);
+  console.log(req.body.cityName)
+});
+
+interface City {
+  city: string;
+  id: string;
+}
+
+// * BONUS TODO: DELETE city from search history
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  let searchHistory = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  let updatedHistory = searchHistory.filter((city: City) => city.id !== req.params.id);
+  fs.writeFileSync('./db/db.json', JSON.stringify(updatedHistory));
+  res.json(updatedHistory);
+});
+
+export default router;
